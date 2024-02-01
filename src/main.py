2de6a7954f4d2833fbfff305c2fc85fbceed795c
dany9999 <daniele99abbagnale@gmail.com>
2024-02-01 18:29:14 +0100

    timer

--- src/main.py
+++ src/main.py
@@ -35,28 +35,29 @@ track_history = {}    # Define a empty dictionary to store the previous center l
 
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')  # Puoi scegliere un altro codec se necessario
-output_video = cv2.VideoWriter('results/output_video.avi', fourcc, 10.0, (1920, 1080))  # Imposta la risoluzione desiderata
+# output_video = cv2.VideoWriter('results/output_video.avi', fourcc, 10.0, (1920, 1080))  # Imposta la risoluzione desiderata
 
 
 previous_roi_status = [{}, {}]
 people_dict = {}
 rois = hf.parse_config_file("data/config.txt", cap)
-
+tt_s = time.perf_counter()
 count = 0
 while cap.isOpened():
           
     success, img = cap.read() # Read the image frame from data source
-
+    
     if not success:
         break    
-    if count % 5 == 0: 
+    if count % 3 == 0: 
         start_time = time.perf_counter()    #Start Timer - needed to calculate FPS        
         # Object Detection
         results = object_detector.run_yolo(img)  # run the yolo v5 object detector 
         detections , num_objects= object_detector.extract_detections(results, img, height=img.shape[0], width=img.shape[1]) # Plot the bounding boxes and extract detections (needed for DeepSORT) and number of relavent objects detected
         # Object Tracking
         tracks_current = tracker.object_tracker.update_tracks(detections, frame=img)
-        
+
+       
         #PAR detection
         id_PAR_label = par_detector.par_detection(tracks_current, img)
         #tracker.display_track(track_history , tracks_current , img)
@@ -83,23 +84,23 @@ while cap.isOpened():
         # cv2.putText(img, f'DETECTED OBJECTS: {num_objects}', (20,120), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
         #hf.display_rois(img, rois)
         cv2.imshow('video_show',img)
-        output_video.write(img)
+        # output_video.write(img)
     
    
     if cv2.waitKey(1) & 0xFF == 27:
         break
     count = count +1 
 
-tf = open("results/PAR_pred_duke.json", "w")
-json.dump(par_detector.id_PAR_label, tf, indent= 2)
-tf.close()    
+# tf = open("results/PAR_pred_duke.json", "w")
+# json.dump(par_detector.id_PAR_label, tf, indent= 2)
+# tf.close()    
 
 
 
 
 # Release and destroy all windows before termination
 cap.release()
-output_video.release()
+# output_video.release()
 cv2.destroyAllWindows()
 
 hf.set_person_attributes(people_dict, par_detector.PAR_common_solution())
@@ -108,7 +109,9 @@ hf.create_the_output_file(people_dict, 'results/results.json')
 
 
 
-print(par_detector.PAR_common_solution())
+tt_end = time.perf_counter()
+
+print(tt_end - tt_s)
 
 
 
